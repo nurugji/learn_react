a994c9b8f10c03dd2b74f0faf0bc57b2c437e916
pingu <nurugji@gmail.com>
2021-10-21 00:09:31 +0900

    How to use state in function(Hook)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -12,11 +12,36 @@ function App() {
 }
 
 function FuncComp(props) {
-  console.log(props);
+  const numberState = useState(props.initNumber);
+  const number = numberState[0];
+  const setNumber = numberState[1];
+
+  // const dateState = useState(new Date().toString());
+  // const _date = dateState[0];
+  // const setDate = dateState[1];
+
+  const [_date, setDate] = useState(new Date().toString());
+  console.log(numberState);
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>Number : {props.initNumber}</p>
+      <p>Number(use state) : {number}</p>
+      <p>Date : {_date}</p>
+      <input
+        type="button"
+        value="random"
+        onClick={function () {
+          setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
+        }}
+      ></input>
     </div>
   );
 }
@@ -24,6 +49,7 @@ function FuncComp(props) {
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
+    date: new Date().toString(),
   };
   render() {
     return (
@@ -31,12 +57,22 @@ class ClassComp extends React.Component {
         <h2>class style component</h2>
         <p>Number : {this.props.initNumber}</p>
         <p>Number(use state) : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={function () {
             this.setState({
               number: Math.random(),
+            });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({
+              date: new Date().toString(),
             });
           }.bind(this)}
         ></input>
